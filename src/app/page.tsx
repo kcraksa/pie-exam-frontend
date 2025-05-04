b3db1f55e1c38a5e8b4b3215c4e6a585b084bc58
Krisna Cipta Raksa <krisna.cipta@sera.astra.co.id>
2025-05-04 19:33:34 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,61 @@
-import Image from "next/image";
 
-export default function Home() {
+import Image from 'next/image';
+
+export default async function Page() {
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_API_URL}/api/pie-page?populate=*`, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
+    },
+  });
+  const {data} = await res.json();
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <section className="py-16 px-6 lg:px-32 bg-white h-full">
+      <div className="text-center mb-10">
+        <p className="text-gray-600">{data?.title}</p>
+        <h2 className="text-3xl font-bold mt-2">{data?.headline}</h2>
+        <p className="text-gray-500 mt-4 mx-auto">
+          {data?.description}
+        </p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      <div className="flex flex-col sm:flex-row items-center justify-between gap-10 w-full">
+        {/* Left Image & Description */}
+        <div className="flex items-center gap-0 relative">
+          {/* Left Circle (Text) */}
+          <div className="w-[200px] h-[200px] sm:w-[200px] sm:h-[200px] md:w-[250px] md:h-[250px] lg:w-[300px] lg:h-[300px] bg-rose-700 opacity-40 rounded-full z-20 flex flex-col justify-center items-start text-white p-4">
+            <h3 className="text-lg font-bold mb-2">{data?.expertize?.title_of_expertize}</h3>
+            <p className="text-xs lg:text-sm text-left">
+              {data?.expertize?.description}
+            </p>
+          </div>
+
+          {/* Right Circle (Image) */}
+          <div className="-ml-12 w-[200px] h-[200px] sm:w-[200px] sm:h-[200px] md:w-[250px] md:h-[250px] lg:w-[300px] lg:h-[300px] rounded-full overflow-hidden z-10 shadow-lg">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/woman-working.jpg"
+              alt="Working Woman"
+              className="w-full h-full object-cover"
+              width={250}
+              height={250}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+        {/* Right Buttons */}
+        <div className="w-full lg:w-1/2 space-y-4">
+          {data?.expertize_points?.map((point: { id: string, point: string }) => (
+            <button
+              key={point.point}
+              className={`w-full flex items-center justify-between px-6 py-4 rounded-full transition-colors bg-gray-300 text-gray-800 hover:bg-rose-700 hover:text-white`}
+            >
+              <span className="text-xl">&lt;</span>
+              <span className="font-semibold">{point.point}</span>
+            </button>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
